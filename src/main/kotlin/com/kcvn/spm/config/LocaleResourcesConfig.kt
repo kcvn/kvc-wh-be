dbd116caad629da9e29fd25e3d4d08db6f5dafaf
hung.tt_3si <hung.tt@3si.vn>
2023-12-26 16:07:16 +0700

    complete internationalization

--- src/main/kotlin/com/kcvn/spm/config/LocaleResourcesConfig.kt
+++ src/main/kotlin/com/kcvn/spm/config/LocaleResourcesConfig.kt
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.i18n.CookieLocaleResolver
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
 import java.util.*
 
+
 @Configuration
-class LocaleResourcesConfig {
+class LocaleResourcesConfig : WebMvcConfigurer {
     @Bean
     fun localeResolver(): LocaleResolver {
         val localeResolver = CookieLocaleResolver()
@@ -31,5 +34,9 @@ class LocaleResourcesConfig {
         messageSource.setBasenames("messages")
         messageSource.setDefaultEncoding("UTF-8")
         return messageSource
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(localeChangeInterceptor())
     }
 }

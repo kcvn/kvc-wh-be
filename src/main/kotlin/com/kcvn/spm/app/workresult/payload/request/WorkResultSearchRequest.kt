4fa535e608f99523f14af41ce44aaed188d321c2
3SI-L122-GIANGN\giang.nt1_3si <giang.nt1>
2024-02-06 11:03:13 +0700

    Update api get list work result.

--- src/main/kotlin/com/kcvn/spm/app/workresult/payload/request/WorkResultSearchRequest.kt
+++ src/main/kotlin/com/kcvn/spm/app/workresult/payload/request/WorkResultSearchRequest.kt
@@ -3,12 +3,12 @@ package com.kcvn.spm.app.workresult.payload.request
 import java.time.LocalDateTime
 
 class WorkResultSearchRequest {
-    val order : String ? = null
-    val itemName : String? = null
-    val listProcessGroup : List<String>? = null
-    val listProcessName : List<String>? = null
-    val tapeLot : String? = null
-    val code : String? = null
-    val fromDate : LocalDateTime? = null
-    val toDate : LocalDateTime? = null
+    var order : String ? = null
+    var itemName : String? = null
+    var listProcessGroup : Array<String>? = null
+    var listProcessName : Array<String>? = null
+    var tapeLot : String? = null
+    var code : String? = null
+    var fromDate : LocalDateTime? = null
+    var toDate : LocalDateTime? = null
 }

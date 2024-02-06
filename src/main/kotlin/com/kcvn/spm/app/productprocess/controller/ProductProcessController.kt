6e4385b39100bad4dd49cac7e4271e0aa4e61c90
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-02-06 14:12:35 +0700

    export excel

--- src/main/kotlin/com/kcvn/spm/app/productprocess/controller/ProductProcessController.kt
+++ src/main/kotlin/com/kcvn/spm/app/productprocess/controller/ProductProcessController.kt
@@ -1,6 +1,7 @@
 package com.kcvn.spm.app.productprocess.controller
 
 import com.kcvn.spm.app.auth.payload.request.RoleRequest
+import com.kcvn.spm.app.product.payload.request.ProductSearchRequest
 import com.kcvn.spm.app.productprocess.payload.request.ProductProcessSearchRequest
 import com.kcvn.spm.app.productprocess.payload.request.UpdateProductProcessDetailRequest
 import com.kcvn.spm.app.productprocess.payload.response.ProductProcessResponse
@@ -8,6 +9,7 @@ import com.kcvn.spm.common.payload.BasePagingResponse
 import com.kcvn.spm.common.payload.BaseResponse
 import com.kcvn.spm.common.payload.MessageResponse
 import com.kcvn.spm.common.payload.PaginatedResponse
+import com.kcvn.spm.common.payload.model.FileContentModel
 import com.kcvn.spm.common.util.CommonUtils
 import com.kcvn.spm.model.tables.pojos.ProductProcess
 import com.kcvn.spm.sample.service.ProductProcessService
@@ -16,7 +18,9 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.data.web.SortDefault
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
@@ -30,7 +34,12 @@ class ProductProcessController(
     @GetMapping("/all")
     fun getAllProductProcess (
         request: ProductProcessSearchRequest,
-        @PageableDefault(size = 10, page = 0, sort = ["processName","layerCode","processSequence"], direction = Sort.Direction.ASC)
+        @PageableDefault(size = 10, page = 0)
+        @SortDefault.SortDefaults(
+            SortDefault(sort = ["processName"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["layerCode"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["processSequence"], direction = Sort.Direction.ASC)
+        )
         pageable: Pageable?
     ): ResponseEntity<BasePagingResponse<ProductProcessResponse>>
     {
@@ -50,4 +59,48 @@ class ProductProcessController(
         return ResponseEntity(BaseResponse(data = productProcess, message = CommonUtils.getMessage("update.succeeded")),HttpStatus.OK)
     }
 
+    @GetMapping("/export-excel")
+    fun exportExcel(
+        request: ProductProcessSearchRequest,
+        @PageableDefault(size = 1000000, page = 0)
+        @SortDefault.SortDefaults(
+            SortDefault(sort = ["processName"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["layerCode"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["processSequence"], direction = Sort.Direction.ASC)
+        )
+        pageable: Pageable
+    ): ResponseEntity<BaseResponse<FileContentModel>> {
+        val data = productProcessService.exportExcel(
+            request.search,
+            request.hasProcessConvertCode,
+            pageable
+        )
+        return ResponseEntity(data, HttpStatus.OK)
+    }
+
+    @PostMapping("/xls")
+    fun generateXlsReport(
+        request: ProductProcessSearchRequest,
+        @PageableDefault(size = 1000000, page = 0)
+        @SortDefault.SortDefaults(
+            SortDefault(sort = ["processName"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["layerCode"], direction = Sort.Direction.ASC),
+            SortDefault(sort = ["processSequence"], direction = Sort.Direction.ASC)
+        )
+        pageable: Pageable)
+    : ResponseEntity<ByteArray> {
+        val report = productProcessService.exportExcel(request.search,
+            request.hasProcessConvertCode,
+            pageable);
+
+        return createResponseEntity(report.data?.content, "test.xls")
+    }
+    private fun createResponseEntity(
+        report: ByteArray?,
+        fileName: String
+    ): ResponseEntity<ByteArray> =
+        ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
+            .body(report)
 }
--- src/main/kotlin/com/kcvn/spm/app/productprocess/service/ProductProcessService.kt
+++ src/main/kotlin/com/kcvn/spm/app/productprocess/service/ProductProcessService.kt
@@ -1,17 +1,29 @@
 package com.kcvn.spm.sample.service
 
+import com.kcvn.spm.app.product.payload.request.ProductSearchRequest
+import com.kcvn.spm.app.productprocess.payload.request.ProductProcessSearchRequest
 import com.kcvn.spm.app.productprocess.payload.request.UpdateProductProcessDetailRequest
 import com.kcvn.spm.app.productprocess.payload.response.ProductProcessResponse
 import com.kcvn.spm.common.exception.BusinessException
 import com.kcvn.spm.common.payload.BasePagingResponse
+import com.kcvn.spm.common.payload.BaseResponse
 import com.kcvn.spm.common.payload.PaginatedResponse
+import com.kcvn.spm.common.payload.model.FileContentModel
 import com.kcvn.spm.common.util.CommonUtils
 import com.kcvn.spm.model.tables.pojos.ProductProcess
 import com.kcvn.spm.repository.ProductProcessRepository
 import com.kcvn.spm.repository.ProductRepository
+import org.apache.poi.ss.usermodel.BorderStyle
+import org.apache.poi.ss.usermodel.CellStyle
+import org.apache.poi.ss.usermodel.Font
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
 
 @Service
 @Transactional
@@ -65,5 +77,70 @@ class ProductProcessService(
             dataResult.add(data)
         }
         return  dataResult
+    }
+
+    fun exportExcel(search: String?, hasProcessConvertCode: Boolean, pageable: Pageable) : BaseResponse<FileContentModel> {
+        val products = productProcessRep.findByKeywordPaginated(search,hasProcessConvertCode, pageable)
+
+        val fileTemplate = File("${System.getProperty("user.dir")}/target/classes/assets/template/ExportProductProcessTemplate.xlsx")
+        val workbook = FileInputStream(fileTemplate).use { x -> XSSFWorkbook(x) }
+        val sheet = workbook.getSheetAt(0)
+
+        if (products.first.isNotEmpty()) {
+            val style: CellStyle = workbook.createCellStyle()
+            style.borderBottom = BorderStyle.THIN
+            style.borderTop = BorderStyle.THIN
+            style.borderRight = BorderStyle.THIN
+            style.borderLeft = BorderStyle.THIN
+            style.wrapText = true
+
+            val font: Font = workbook.createFont()
+            font.fontName = "Times New Roman"
+            font.fontHeightInPoints = 12.toShort()
+            style.setFont(font)
+
+
+            var rowNumber = 2
+            for (item in products.first) {
+                val dataRow: Row = sheet.createRow(rowNumber++)
+                dataRow.createCell(0).setCellValue(item.productName)
+                dataRow.getCell(0).cellStyle = style
+
+                dataRow.createCell(1).setCellValue(item.layerCode)
+                dataRow.getCell(1).cellStyle = style
+
+                dataRow.createCell(2).setCellValue(item.processCode)
+                dataRow.getCell(2).cellStyle = style
+
+                dataRow.createCell(3).setCellValue(item.processName)
+                dataRow.getCell(3).cellStyle = style
+
+                dataRow.createCell(4).setCellValue(item.processNameJp)
+                dataRow.getCell(4).cellStyle = style
+
+                dataRow.createCell(5).setCellValue(item.processConvertCode)
+                dataRow.getCell(5).cellStyle = style
+
+                dataRow.createCell(6).setCellValue(item.processInventoryCode)
+                dataRow.getCell(6).cellStyle = style
+
+                dataRow.createCell(7).setCellValue(item.processStatisticCode)
+                dataRow.getCell(7).cellStyle = style
+            }
+        }
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        workbook.write(byteArrayOutputStream)
+
+        val excelBytes = byteArrayOutputStream.toByteArray()
+
+        val response = FileContentModel(
+            fileName = "Danh_sach_cong_doan.xlsx",
+            contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            content = excelBytes
+        )
+
+        workbook.close()
+
+        return BaseResponse(response)
     }
 }

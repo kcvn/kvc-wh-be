73bec54aeecf40621dd28cf4b54fb0ecd660a100
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-02-16 17:38:24 +0700

    commit

--- src/main/kotlin/com/kcvn/spm/app/productprocess/controller/ProductProcessController.kt
+++ src/main/kotlin/com/kcvn/spm/app/productprocess/controller/ProductProcessController.kt
@@ -60,6 +60,8 @@ class ProductProcessController(
         )
     }
 
+    
+
     @GetMapping("/export-excel")
     @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).E_PROCESS.value) || hasRole('ADMIN')")
     fun exportExcel(

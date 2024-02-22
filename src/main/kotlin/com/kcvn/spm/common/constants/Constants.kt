e59470b163617f76972ab903fd1205e6f00a5b76
canhnd3si <canh.nd@3si.vn>
2024-02-22 13:11:32 +0700

    fix

--- src/main/kotlin/com/kcvn/spm/common/constants/Constants.kt
+++ src/main/kotlin/com/kcvn/spm/common/constants/Constants.kt
@@ -26,5 +26,17 @@ class Constants {
         //Excel
         const val EXCEL_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         const val FONT_TIMES_NEW_ROMAN = "Times New Roman"
+
+        //KHUON_DUC
+        const val KHUONDUC_KVC = "KVC"
+        const val KHUONDUC_ML = "ML"
+        const val KHUONDUC_SKE = "SKE"
+        const val KHUONDUC_SWR = "SWR"
+        const val KHUONDUC_SUR = "SUR"
+
+        //KHUNG 1
+        const val KHUNG1_MU = "MU"
+        const val KHUNG1_ML = "ML"
+        const val KHUNG1_SWR = "SWR"
     }
 }

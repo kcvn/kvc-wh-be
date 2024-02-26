9e75279431b5ac5ea65562d671c2e8b9f495fbae
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-02-26 15:05:10 +0700

    commit

--- src/main/kotlin/com/kcvn/spm/app/productprocess/service/ProductProcessService.kt
+++ src/main/kotlin/com/kcvn/spm/app/productprocess/service/ProductProcessService.kt
@@ -55,7 +55,7 @@ class ProductProcessService(
                     processSequence = productProcess.processSequence
                 );
             }
-        response.data = (response.data as List<ProductProcessResponse?>).sortedWith(compareBy<ProductProcessResponse?> {it?.productName}.thenBy { it?.layerCodeInt }.thenBy { it?.processSequence })
+            response.data = (response.data as List<ProductProcessResponse?>).sortedWith(compareBy<ProductProcessResponse?> {it?.productName}.thenBy { it?.layerCodeInt }.thenBy { it?.processSequence })
             response.totalRecords = result.second ?: 0
 
         return response;

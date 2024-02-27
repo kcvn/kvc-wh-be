52529c29d5e326d2ea075c4e6c70c9651d0fd65f
iphongnv0910 <i_phong.nv@3si.vn>
2024-02-27 11:32:41 +0700

    hot fix orer list

--- src/main/kotlin/com/kcvn/spm/app/order/payload/response/OrderCodeResponse.kt
+++ src/main/kotlin/com/kcvn/spm/app/order/payload/response/OrderCodeResponse.kt
@@ -3,7 +3,7 @@ package com.kcvn.spm.app.order.payload.response
 import com.kcvn.spm.common.payload.DropdownResponse
 
 data class OrderCodeResponse(
-    var key: String? = null,
+    var value: String? = null,
     var label: String? = null,
     val listVersion: List<DropdownResponse>
 )

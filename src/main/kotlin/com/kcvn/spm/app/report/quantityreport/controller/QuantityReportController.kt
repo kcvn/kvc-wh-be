9ea9a5e21d1e56f4f68fac8148cc020a3093ecb5
3SI-L122-GIANGN\giang.nt1_3si <giang.nt1>
2024-03-01 17:50:02 +0700

    create api get list calculate quantity report

--- src/main/kotlin/com/kcvn/spm/app/report/quantityreport/controller/QuantityReportController.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/quantityreport/controller/QuantityReportController.kt
@@ -2,8 +2,11 @@ package com.kcvn.spm.app.report.quantityreport.controller
 
 import com.kcvn.spm.app.report.quantityreport.payload.request.CalculateQuantityRequest
 import com.kcvn.spm.app.report.quantityreport.service.QuantityReportService
+import com.kcvn.spm.common.payload.BasePagingResponse
 import com.kcvn.spm.common.payload.BaseResponse
+import com.kcvn.spm.model.tables.pojos.CalculateQuantityResult
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.data.web.SortDefault
 import org.springframework.http.HttpStatus
@@ -20,22 +23,29 @@ class QuantityReportController(
     private val quantityReportService: QuantityReportService,
 ) {
     @PostMapping("/calculate-quantity")
-    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).V_WORK_RESULT.value) || hasRole('ADMIN')")
-    fun CalculateQuantity(
-        request: CalculateQuantityRequest,
-        @PageableDefault(size = 10, page = 0)
-        @SortDefault.SortDefaults(
-        ) pageable: Pageable,
-    ): ResponseEntity<BaseResponse<Boolean>> {
+    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).CA_REPORT_QUANTITY.value) || hasRole('ADMIN')")
+    fun CalculateQuantity(request: CalculateQuantityRequest): ResponseEntity<BaseResponse<Boolean>> {
         val data = quantityReportService.calculateQuantity(request)
         return ResponseEntity<BaseResponse<Boolean>>(data, HttpStatus.OK)
     }
 
     @GetMapping("/locked-quantity")
-    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).V_WORK_RESULT.value) || hasRole('ADMIN')")
-    fun LockedQuantity(request: String) : ResponseEntity<BaseResponse<Boolean>> {
+    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).LOCK_REPORT_QUANTITY.value) || hasRole('ADMIN')")
+    fun LockedQuantity(request: String): ResponseEntity<BaseResponse<Boolean>> {
         val result = quantityReportService.lockedQuantity(request)
-        return ResponseEntity<BaseResponse<Boolean>>(result,HttpStatus.OK)
+        return ResponseEntity<BaseResponse<Boolean>>(result, HttpStatus.OK)
+    }
+
+    @GetMapping("/get-list-calculate-quantity-result")
+    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).V_REPORT_QUANTITY.value) || hasRole('ADMIN')")
+    fun getListCalculateQuantityResult(
+        @PageableDefault(size = 10, page = 0)
+        @SortDefault.SortDefaults(
+            SortDefault(sort = ["monthReport"], direction = Sort.Direction.DESC)
+        ) pageable: Pageable,
+    ): ResponseEntity<BasePagingResponse<CalculateQuantityResult>> {
+        val result = quantityReportService.getListCalculateQuantityResult(pageable)
+        return ResponseEntity<BasePagingResponse<CalculateQuantityResult>>(result, HttpStatus.OK)
     }
 
 }

7bad907c8e1c67d5a35a43d55990f885078a3a6b
iphongnv0910 <i_phong.nv@3si.vn>
2024-03-06 11:36:52 +0700

    fix messsage completionrate

--- src/main/kotlin/com/kcvn/spm/app/completionrate/payload/model/LayerImportCompletionRateProductModel.kt
+++
@@ -1,9 +0,0 @@
-package com.kcvn.spm.app.completionrate.payload.model
-
-import java.math.BigInteger;
-
-class LayerImportCompletionRateProductModel {
-    var key: String? = null
-    var rate: BigInteger? = null
-    var errorMessage: String? =null
-}

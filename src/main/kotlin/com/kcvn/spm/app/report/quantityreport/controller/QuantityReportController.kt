0699f38c8c6f8fcf3477db4ae6b950b29096132f
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-03-07 17:12:51 +0700

    fix bao cao san luong

--- src/main/kotlin/com/kcvn/spm/app/report/quantityreport/controller/QuantityReportController.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/quantityreport/controller/QuantityReportController.kt
@@ -1,11 +1,15 @@
 package com.kcvn.spm.app.report.quantityreport.controller
 
+import com.kcvn.spm.app.inventoryproduct.payload.response.CheckInventoryDateResponse
 import com.kcvn.spm.app.report.quantityreport.payload.request.CalculateQuantityRequest
 import com.kcvn.spm.app.report.quantityreport.payload.request.QuantityReportRequest
+import com.kcvn.spm.app.report.quantityreport.payload.response.CheckCalculateQuantityResponse
 import com.kcvn.spm.app.report.quantityreport.payload.response.PagingQuantityReportResponse
 import com.kcvn.spm.app.report.quantityreport.service.QuantityReportService
+import com.kcvn.spm.common.helper.DateTimeHelper
 import com.kcvn.spm.common.payload.BasePagingResponse
 import com.kcvn.spm.common.payload.BaseResponse
+import com.kcvn.spm.common.util.CommonUtils
 import com.kcvn.spm.model.tables.pojos.CalculateQuantityResult
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.OffsetDateTime
 
 @RestController
 @RequestMapping("/api/quantity-report")
@@ -62,5 +67,25 @@ class QuantityReportController(
     ) : ResponseEntity<PagingQuantityReportResponse>{
         val data = quantityReportService.getListQuantityReport(request,pageable)
         return ResponseEntity<PagingQuantityReportResponse>(data,HttpStatus.OK)
+    }
+
+    @GetMapping("/check-quantity-report")
+    fun checkInventoryDate(request: CalculateQuantityRequest
+    ) : ResponseEntity<BaseResponse<CheckCalculateQuantityResponse>>{
+        val data = quantityReportService.checkCalculateQuantity(request)
+
+        val month = request.endDate?.monthValue
+        val year = request.endDate?.year
+        return if(data.hasCalculateQuantity ){
+            ResponseEntity(
+                BaseResponse(data = data, message = CommonUtils.getMessage("quantity.validation",arrayOf(month.toString(), year.toString()))),
+                HttpStatus.OK
+            )
+        }else{
+            ResponseEntity(
+                BaseResponse(data = data, message = "Ok"),
+                HttpStatus.OK
+            )
+        }
     }
 }
--- src/main/kotlin/com/kcvn/spm/common/helper/DateTimeHelper.kt
+++ src/main/kotlin/com/kcvn/spm/common/helper/DateTimeHelper.kt
@@ -63,5 +63,6 @@ class DateTimeHelper {
         fun toUniversalTime(date: LocalDateTime): OffsetDateTime {
             return OffsetDateTime.of(date, ZoneOffset.UTC).plusHours(-7)
         }
+
     }
 }

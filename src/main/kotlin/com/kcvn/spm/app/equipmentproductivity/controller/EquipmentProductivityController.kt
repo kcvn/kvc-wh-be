5c9c89e36432ab88b9bf5eff2bc3c95995d525da
iphongnv0910 <i_phong.nv@3si.vn>
2024-03-08 15:16:31 +0700

    con fig api response

--- src/main/kotlin/com/kcvn/spm/app/equipmentproductivity/controller/EquipmentProductivityController.kt
+++ src/main/kotlin/com/kcvn/spm/app/equipmentproductivity/controller/EquipmentProductivityController.kt
@@ -21,12 +21,12 @@ import org.springframework.web.multipart.MultipartFile
 import java.time.OffsetDateTime
 
 @RestController
-@RequestMapping("/api/machine-productivity")
+@RequestMapping("/api/plan")
 class EquipmentProductivityController(
     private val equipmentProductivityService: EquipmentProductivityService)
 {
 
-    @GetMapping("/get-list")
+    @GetMapping("/equipment-productivity/get-list")
     //@PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).V_ORDER.value) || hasRole('ADMIN')")
     fun getList(
         request: PlanSearchRequest?,
@@ -61,14 +61,6 @@ class EquipmentProductivityController(
             fakeProcessDetail.add(processDetailModel)
         }
 
-        // Tạo dữ liệu giả cho EquipmentProductivityModel
-        val fakeEquipmentProductivityModel = EquipmentProductivityModel(
-            frame1 = "Frame1",
-            processName = "ProcessName",
-            processNameJp = "ProcessNameJP",
-            processConvertCode = "ConvertCode",
-            processDetail = fakeProcessDetail
-        )
 
         // Tạo danh sách giả cho EquipmentProductivityModel
         val fakeEquipmentProductivityModelList: MutableList<EquipmentProductivityModel> = mutableListOf()
@@ -91,7 +83,7 @@ class EquipmentProductivityController(
 
 
 
-    @PostMapping(value = ["/product/import-excel"], consumes = ["multipart/form-data"])
+    @PostMapping(value = ["/equipment-productivity/import-excel"], consumes = ["multipart/form-data"])
 //    @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).I_COMPLETION_RATE.value) || hasRole('ADMIN')")
     fun importExcel(
         @RequestPart("file") file: MultipartFile,

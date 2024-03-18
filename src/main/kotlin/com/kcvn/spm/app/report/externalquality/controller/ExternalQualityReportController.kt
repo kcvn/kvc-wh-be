837bc6bc6eabc200146d7b3da7dce6e35bcf39aa
canhnd3si <canh.nd@3si.vn>
2024-03-18 11:13:22 +0700

    fix

---
+++ src/main/kotlin/com/kcvn/spm/app/report/externalquality/controller/ExternalQualityReportController.kt
@@ -0,0 +1,9 @@
+package com.kcvn.spm.app.report.externalquality.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/report/external-quality")
+class ExternalQualityReportController {
+}
---
+++ src/main/kotlin/com/kcvn/spm/app/report/externalquality/payload/model/ExternalQualityReportModel.kt
@@ -0,0 +1,5 @@
+package com.kcvn.spm.app.report.externalquality.payload.model
+
+data class ExternalQualityReportModel(
+    var productName: String? = null
+)

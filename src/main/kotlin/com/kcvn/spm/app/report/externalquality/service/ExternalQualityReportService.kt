27ecae30e2072a89e2eb3e567af42fc9ed1d2fcb
iphongnv0910 <i_phong.nv@3si.vn>
2024-03-20 09:44:55 +0700

    hot fix export excel quantity report

--- src/main/kotlin/com/kcvn/spm/app/report/externalquality/service/ExternalQualityReportService.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/externalquality/service/ExternalQualityReportService.kt
@@ -28,22 +28,40 @@ class ExternalQualityReportService(
         val orderSearchRequest = OrderSearchRequest()
         orderSearchRequest.endDate = request.endDate
         orderSearchRequest.startDate = request.startDate
+        orderSearchRequest.productName = request.productName
         val order = orderRep.getPagingListOrder(orderSearchRequest,pageable)
-        val listProduct = order.first
-        val mappingPaging: List<ExternalQualityReportModel> = listProduct.map { product ->
+        val listProductOrder = order.first
+        val mappingPaging: List<ExternalQualityReportModel> = listProductOrder.map { product ->
             val externalQuality = ExternalQualityReportModel()
             externalQuality.productName = product.productName
-            externalQuality.blockSh = product.shBlock
             externalQuality.pcsSh = product.pcsSh
             externalQuality.productShortcutName = product.productShortcutName
             externalQuality
         }
-        val listProductName = listProduct.map { it.productName }
+        val listProductName = listProductOrder.map { it.productName }
+
+        val listProduct = productRep.getByName(listProductName.filterNotNull())
         val listCompletionRate = completionRateProductRep.getByProduct(listProductName.filterNotNull())
 
+        for(externalQuality in mappingPaging){
+            if (listCompletionRate != null) {
+                externalQuality.completionRate = listCompletionRate.find { x-> x.productName.equals(externalQuality.productName) }?.rate
+            }
+        }
+
+        for(externalQuality in mappingPaging){
+                val product = listProduct.find { x-> x.name.equals(externalQuality.productName) }
+                if(product !=null){
+                    externalQuality.mold = product.mold
+                    externalQuality.snapMold = product.snapMold
+                    externalQuality.pcsSh = product.pcsSh
+                    externalQuality.tapeCommon = product.tapeCommon
+                    externalQuality.exportType = product.exportType
+                    externalQuality.blockSh = product.shBlock
+                }
+        }
+
         response.data = mappingPaging
-
-
 
         return response
     }

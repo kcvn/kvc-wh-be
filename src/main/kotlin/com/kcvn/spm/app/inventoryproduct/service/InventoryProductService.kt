f18f8a4466f8e6b6418311275587731079d7741b
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-03-25 13:43:12 +0700

    fix invetory

--- src/main/kotlin/com/kcvn/spm/app/inventoryproduct/service/InventoryProductService.kt
+++ src/main/kotlin/com/kcvn/spm/app/inventoryproduct/service/InventoryProductService.kt
@@ -83,8 +83,8 @@ class InventoryProductService(
         val templateUrl = "${System.getProperty("user.dir")}/target/classes/assets/template/ImportInventoryProductTemplate.xlsx"
         if (ExcelHelper.fileIsEmpty(sheet, rowIndex)) throw BusinessException(CommonUtils.getMessage("import.file.empty"))
 
-        if (!ExcelHelper.columnIsMatchingTemplate(templateUrl, headerRow, 0, 8))
-            throw BusinessException(CommonUtils.getMessage("validate.excel.invalidFormat"))
+//        if (!ExcelHelper.columnIsMatchingTemplate(templateUrl, headerRow, 0, 8))
+//            throw BusinessException(CommonUtils.getMessage("validate.excel.invalidFormat"))
 
         val requestDelete = InventoryProduct()
         requestDelete.inventoryDate = date
@@ -264,11 +264,12 @@ class InventoryProductService(
                     if (filterCheckProcessProcedure == null) {
                         messageResults.add(CommonUtils.getMessage("validate.excel.inventoryProduct.dataNull"))
                     } else {
+                        var test = ExcelHelper.getCellValue(row, 2).toBigDecimalOrNull();
                         val requestImport = InventoryProduct(
                             processProcedureStructureId = filterCheckProcessProcedure.id,
                             inventoryDate = date,
                             code = if (ExcelHelper.getCellValue(row, 2).toBigDecimalOrNull() != null) {
-                                ExcelHelper.getCellValue(row, 2).toBigDecimalOrNull()?.toInt().toString()
+                                ExcelHelper.getCellValue(row, 2).toBigDecimalOrNull().toString()
                             } else {
                                 ExcelHelper.getCellValue(row, 2)
                             },
@@ -277,7 +278,6 @@ class InventoryProductService(
                             productQuantity = ExcelHelper.getCellValue(row, 7).toBigDecimalOrNull()?.toInt(),
                             sheetQuantity = ExcelHelper.getCellValue(row, 8).toBigDecimalOrNull()?.toInt()
                         )
-
                         val checkInventoryProduct = inventoryProductRepository.findInventoryProduct(filterCheckProcessProcedure.id, date, requestImport.code ?: "")
 
                         if (checkInventoryProduct == null) {

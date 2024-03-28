28de0572b47a96ef030c0347d94ae9e9a28e307a
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-03-28 15:03:10 +0700

    update get all api bc nvl

--- src/main/kotlin/com/kcvn/spm/app/report/materials/payload/request/GetReportMaterialsRequest.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/materials/payload/request/GetReportMaterialsRequest.kt
@@ -1,4 +1,11 @@
 package com.kcvn.spm.app.report.materials.payload.request
 
+import java.time.OffsetDateTime
+
 class GetReportMaterialsRequest {
+    var startTime: OffsetDateTime? = null
+    var endTime: OffsetDateTime? = null
+    var productName: String? = null
+    var tapeShared: String? = null
+    var typeTape: String? = null
 }

08b63c7639039144dad982e39c592bd160adc517
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-04-11 13:38:58 +0700

    commit

--- src/main/kotlin/com/kcvn/spm/app/report/quantityreport/service/QuantityReportService.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/quantityreport/service/QuantityReportService.kt
@@ -118,6 +118,7 @@ class QuantityReportService(
                         value = "test",
                         description = "dayQueryTapePre : ${dayQueryTapePre} - dayReport : ${dayReport}",
                     )
+                    appSettingRepository.add(test)
                     throw BusinessException(CommonUtils.getMessage("validate.importTape.orderRequestDate"))
                 }
             }

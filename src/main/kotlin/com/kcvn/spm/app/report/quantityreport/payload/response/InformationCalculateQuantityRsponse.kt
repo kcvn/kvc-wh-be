5bcac207445646ffcedb1b8c274284988483f45d
anh.dv_3si <anh.dv_3si@3SI-L100-ANHDV>
2024-04-11 10:03:49 +0700

    update fix bug code

--- src/main/kotlin/com/kcvn/spm/app/report/quantityreport/payload/response/InformationCalculateQuantityRsponse.kt
+++ src/main/kotlin/com/kcvn/spm/app/report/quantityreport/payload/response/InformationCalculateQuantityRsponse.kt
@@ -1,4 +1,14 @@
 package com.kcvn.spm.app.report.quantityreport.payload.response
 
-class InformationCalculateQuantityRsponse {
-}
+import java.time.OffsetDateTime
+
+data class InformationCalculateQuantityResponse (
+    var monthReport: OffsetDateTime? = null,
+    var productName: String? = null,
+    var processStatistic: String? = null,
+    var totalQuantityOfProcess: Int? = null,
+    var calculateQuantityResultId: String? = null,
+    var monthNumber: Int? = null,
+    var yearNumber: Int? = null,
+    var orderDateFromTo: String? = null
+)

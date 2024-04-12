14944b76bc9281bbacc3ee6dff09a8b7e6f47e3c
iphongnv0910 <i_phong.nv@3si.vn>
2024-04-12 18:19:46 +0700

    fix conflix

--- src/main/kotlin/com/kcvn/spm/app/plan/service/CreatePlanService.kt
+++ src/main/kotlin/com/kcvn/spm/app/plan/service/CreatePlanService.kt
@@ -624,7 +624,7 @@ class CreatePlanService(
     private fun settingEquipmentConfig(equipmentUsedInfo: EquipmentProductivity?, equipmentInfoDefault: EquipmentProductivity): EquipmentProductivity {
         val data = EquipmentProductivity(
             id = equipmentInfoDefault.id,
-            equipmentCode = equipmentInfoDefault.equipmentCode,
+            processName = equipmentInfoDefault.processName,
             description = equipmentInfoDefault.description,
             frame_1 = equipmentInfoDefault.frame_1,
             grpProcess = equipmentInfoDefault.grpProcess,
@@ -652,7 +652,7 @@ class CreatePlanService(
     private fun settingEquipmentConfig(planDetail: PlanDetailCreateModel, equipmentInfoDefault: EquipmentProductivity): EquipmentProductivity {
         val data = EquipmentProductivity(
             id = equipmentInfoDefault.id,
-            equipmentCode = equipmentInfoDefault.equipmentCode,
+            processName = equipmentInfoDefault.processName,
             description = equipmentInfoDefault.description,
             frame_1 = equipmentInfoDefault.frame_1,
             grpProcess = equipmentInfoDefault.grpProcess,

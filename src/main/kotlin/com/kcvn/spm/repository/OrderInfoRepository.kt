e585e7813cd284b25f7697f341f6284256094314
iphongnv0910 <i_phong.nv@3si.vn>
2024-04-19 10:28:37 +0700

    update order get list

--- src/main/kotlin/com/kcvn/spm/repository/OrderInfoRepository.kt
+++ src/main/kotlin/com/kcvn/spm/repository/OrderInfoRepository.kt
@@ -206,7 +206,8 @@ class OrderInfoRepository(private val context: DSLContext) : SortingRepository()
                         x.productName,
                         x.version.toString(),
                         DateTimeHelper.toTimeZone7toString(x.orderDate!!, DateTimeFormat.yyyyMMdd),
-                        x.quantity
+                        x.quantity,
+                        x.isChangeQuantity
                     )
                 }
 
@@ -222,7 +223,9 @@ class OrderInfoRepository(private val context: DSLContext) : SortingRepository()
                     x.key.first,
                     versionByProducts.find { m -> m.first == x.key.first }?.second,
                     x.key.second,
-                    x.value.sumOf { m -> m.quantity ?: 0 }
+                    x.value.sumOf { m -> m.quantity ?: 0 },
+                    x.value.any { m -> m.isHasDifferent == true }
+
                 )
             }
             return data
@@ -234,7 +237,8 @@ class OrderInfoRepository(private val context: DSLContext) : SortingRepository()
                         x.productName,
                         x.version.toString(),
                         DateTimeHelper.toTimeZone7toString(x.orderDate!!, DateTimeFormat.yyyyMMdd),
-                        x.quantity
+                        x.quantity,
+                        x.isChangeQuantity
                     )
                 }
             return data

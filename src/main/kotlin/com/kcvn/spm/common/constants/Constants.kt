527b1e2d290652517b1163595627ff0c5a72b1dd
iphongnv0910 <i_phong.nv@3si.vn>
2024-04-24 11:10:10 +0700

    custom common validate date and allow null condtion for external quality report

--- src/main/kotlin/com/kcvn/spm/common/constants/Constants.kt
+++ src/main/kotlin/com/kcvn/spm/common/constants/Constants.kt
@@ -102,6 +102,8 @@ class DateTimeFormat {
         const val yyyy_MM_dd_HH_mm_ss = "yyyy_MM_dd_HH_mm_ss"
         const val MM_yyyy = "MM/yyyy"
         const val M_dd_yyyy = "M/dd/yyyy"
+        const val yyyy_MM_dd = "yyyy/MM/dd"
+
 
     }
 }

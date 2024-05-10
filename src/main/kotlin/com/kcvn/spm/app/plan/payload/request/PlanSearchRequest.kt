ef358af3322171d694507fbe8c54c6e5ed8de92c
iphongnv0910 <i_phong.nv@3si.vn>
2024-05-10 13:37:51 +0700

    fix request appove

--- src/main/kotlin/com/kcvn/spm/app/plan/payload/request/PlanSearchRequest.kt
+++ src/main/kotlin/com/kcvn/spm/app/plan/payload/request/PlanSearchRequest.kt
@@ -2,13 +2,14 @@ package com.kcvn.spm.app.plan.payload.request
 
 import java.time.OffsetDateTime
 
-data class PlanSearchRequest(
-    var productName: String? = null,
-    var processGroups: String? = null,
-    var frame_1: String? = null,
-    var mold: String? = null,
-    var startDate: OffsetDateTime? = null,
-    var endDate: OffsetDateTime? = null,
-    var inventoryWorkPlan: Boolean? = null,
+class PlanSearchRequest {
+    var productName: String? = null
+    var processGroups: String? = null
+    var frame_1: String? = null
+    var mold: String? = null
+    var startDate: OffsetDateTime? = null
+    var endDate: OffsetDateTime? = null
+    var inventoryWorkPlan: Boolean? = null
     var draftWorkPlan: Boolean? = null
-)
+    var fileName: String? = null
+}

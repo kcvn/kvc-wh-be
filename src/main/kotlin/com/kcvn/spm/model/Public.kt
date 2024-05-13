65c2ef169a257ca230cbb43ca8752134a8fd7673
canhnd3si <canh.nd@3si.vn>
2024-05-13 17:29:30 +0700

    plan

--- src/main/kotlin/com/kcvn/spm/model/Public.kt
+++ src/main/kotlin/com/kcvn/spm/model/Public.kt
@@ -29,6 +29,7 @@ import com.kcvn.spm.model.tables.OrderInfo
 import com.kcvn.spm.model.tables.OrderVersionDropdown
 import com.kcvn.spm.model.tables.Plan
 import com.kcvn.spm.model.tables.PlanCalendarConfig
+import com.kcvn.spm.model.tables.PlanColorConfig
 import com.kcvn.spm.model.tables.PlanDetail
 import com.kcvn.spm.model.tables.PlanDetailTemp
 import com.kcvn.spm.model.tables.PlanProcess
@@ -196,6 +197,11 @@ open class Public : SchemaImpl("public", DefaultCatalog.DEFAULT_CATALOG) {
     val PLAN_CALENDAR_CONFIG: PlanCalendarConfig get() = PlanCalendarConfig.PLAN_CALENDAR_CONFIG
 
     /**
+     * The table <code>public.plan_color_config</code>.
+     */
+    val PLAN_COLOR_CONFIG: PlanColorConfig get() = PlanColorConfig.PLAN_COLOR_CONFIG
+
+    /**
      * The table <code>public.plan_detail</code>.
      */
     val PLAN_DETAIL: PlanDetail get() = PlanDetail.PLAN_DETAIL
@@ -323,6 +329,7 @@ open class Public : SchemaImpl("public", DefaultCatalog.DEFAULT_CATALOG) {
         OrderVersionDropdown.ORDER_VERSION_DROPDOWN,
         Plan.PLAN,
         PlanCalendarConfig.PLAN_CALENDAR_CONFIG,
+        PlanColorConfig.PLAN_COLOR_CONFIG,
         PlanDetail.PLAN_DETAIL,
         PlanDetailTemp.PLAN_DETAIL_TEMP,
         PlanProcess.PLAN_PROCESS,
--- src/main/kotlin/com/kcvn/spm/model/keys/Keys.kt
+++ src/main/kotlin/com/kcvn/spm/model/keys/Keys.kt
@@ -29,6 +29,7 @@ import com.kcvn.spm.model.tables.OrderInfo
 import com.kcvn.spm.model.tables.OrderVersionDropdown
 import com.kcvn.spm.model.tables.Plan
 import com.kcvn.spm.model.tables.PlanCalendarConfig
+import com.kcvn.spm.model.tables.PlanColorConfig
 import com.kcvn.spm.model.tables.PlanDetail
 import com.kcvn.spm.model.tables.PlanDetailTemp
 import com.kcvn.spm.model.tables.PlanProcess
@@ -73,6 +74,7 @@ import com.kcvn.spm.model.tables.records.OrderInfoRecord
 import com.kcvn.spm.model.tables.records.OrderRecord
 import com.kcvn.spm.model.tables.records.OrderVersionDropdownRecord
 import com.kcvn.spm.model.tables.records.PlanCalendarConfigRecord
+import com.kcvn.spm.model.tables.records.PlanColorConfigRecord
 import com.kcvn.spm.model.tables.records.PlanDetailRecord
 import com.kcvn.spm.model.tables.records.PlanDetailTempRecord
 import com.kcvn.spm.model.tables.records.PlanProcessRecord
@@ -131,6 +133,7 @@ val ORDER_INFO_PKEY: UniqueKey<OrderInfoRecord> = Internal.createUniqueKey(Order
 val ORDER_VERSION_DROPDOWN_PKEY: UniqueKey<OrderVersionDropdownRecord> = Internal.createUniqueKey(OrderVersionDropdown.ORDER_VERSION_DROPDOWN, DSL.name("order_version_dropdown_pkey"), arrayOf(OrderVersionDropdown.ORDER_VERSION_DROPDOWN.ID), true)
 val PLAN_PKEY: UniqueKey<PlanRecord> = Internal.createUniqueKey(Plan.PLAN, DSL.name("plan_pkey"), arrayOf(Plan.PLAN.ID), true)
 val PLAN_CALENDAR_CONFIG_PKEY: UniqueKey<PlanCalendarConfigRecord> = Internal.createUniqueKey(PlanCalendarConfig.PLAN_CALENDAR_CONFIG, DSL.name("plan_calendar_config_pkey"), arrayOf(PlanCalendarConfig.PLAN_CALENDAR_CONFIG.ID), true)
+val PLAN_COLOR_CONFIG_PKEY: UniqueKey<PlanColorConfigRecord> = Internal.createUniqueKey(PlanColorConfig.PLAN_COLOR_CONFIG, DSL.name("plan_color_config_pkey"), arrayOf(PlanColorConfig.PLAN_COLOR_CONFIG.ID), true)
 val PLAN_DETAIL_PKEY: UniqueKey<PlanDetailRecord> = Internal.createUniqueKey(PlanDetail.PLAN_DETAIL, DSL.name("plan_detail_pkey"), arrayOf(PlanDetail.PLAN_DETAIL.ID), true)
 val PLAN_DETAIL_TEMP_PKEY: UniqueKey<PlanDetailTempRecord> = Internal.createUniqueKey(PlanDetailTemp.PLAN_DETAIL_TEMP, DSL.name("plan_detail_temp_pkey"), arrayOf(PlanDetailTemp.PLAN_DETAIL_TEMP.ID), true)
 val PLAN_PROCESS_PKEY: UniqueKey<PlanProcessRecord> = Internal.createUniqueKey(PlanProcess.PLAN_PROCESS, DSL.name("plan_process_pkey"), arrayOf(PlanProcess.PLAN_PROCESS.ID), true)

db332a555a934c8b5cdf8f4211e314190f37c34a
canhnd3si <canh.nd@3si.vn>
2024-05-20 14:01:47 +0700

    fix bug

--- src/main/kotlin/com/kcvn/spm/app/masterdata/service/MasterDataService.kt
+++ src/main/kotlin/com/kcvn/spm/app/masterdata/service/MasterDataService.kt
@@ -1,26 +1,11 @@
 package com.kcvn.spm.app.masterdata.service
 
 import com.kcvn.spm.app.masterdata.payload.response.MasterDataSelectionResponse
-import com.kcvn.spm.common.constants.ExcelConstant
 import com.kcvn.spm.common.constants.MasterDataType
-import com.kcvn.spm.common.exception.BusinessException
-import com.kcvn.spm.common.helper.ExcelHelper
-import com.kcvn.spm.common.payload.BaseResponse
 import com.kcvn.spm.common.payload.DropdownResponse
-import com.kcvn.spm.common.payload.model.FileContentModel
-import com.kcvn.spm.common.util.CommonUtils
-import com.kcvn.spm.model.tables.pojos.ProcessMasterData
 import com.kcvn.spm.repository.CommonCategoryRepository
-import org.apache.poi.ss.usermodel.CellType
-import org.apache.poi.ss.usermodel.WorkbookFactory
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
-import java.io.ByteArrayOutputStream
-import java.io.FileInputStream
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @Service
 @Transactional
@@ -40,10 +25,12 @@ class MasterDataService(
             moldSelections = data.filter { x -> x.type == MasterDataType.KHUON_DUC }.map { x -> DropdownResponse(x.value, x.value) },
             srNosrSelections = data.filter { x -> x.type == MasterDataType.SR_OR_NSR }.map { x -> DropdownResponse(x.value, x.value) },
             exportTypeSelections = data.filter { x -> x.type == MasterDataType.LOAI_XUAT_HANG }.map { x -> DropdownResponse(x.value, x.value) },
-            tapeTypeSelections = data.filter { x -> x.type == MasterDataType.LOAI_TAPE }.map { x -> DropdownResponse(x.value, x.value) },
+            tapeTypeSelections = data.filter { x -> x.type == MasterDataType.LOAI_TAPE }
+                .sortedBy { it.value }.map { x -> DropdownResponse(x.value, x.value) },
             processConvertCodes = data.filter { x -> x.type == MasterDataType.MACHUYENDOI }.map { x -> DropdownResponse(x.value, x.value) },
             processStatisticCodes = data.filter { x -> x.type == MasterDataType.MATHONGKE }.map { x -> DropdownResponse(x.value, x.value) },
-            tapeCommonSelections = data.filter { x -> x.type == MasterDataType.TAPE_DUNG_CHUNG }.map { x -> DropdownResponse(x.value, x.value) },
+            tapeCommonSelections = data.filter { x -> x.type == MasterDataType.TAPE_DUNG_CHUNG }
+                .sortedBy { it.value }.map { x -> DropdownResponse(x.value, x.value) },
             ringJigSelections = data.filter { x -> x.type == MasterDataType.RING_JIG }.map { x -> DropdownResponse(x.value, x.value) }
         )
     }

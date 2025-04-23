40d2d8d74ebfd0ee7971b51bd05f60c8671fba79
Cong Tran <cong-tran@kcvn.com.vn>
2025-04-23 16:12:34 +0700

    change querySql in stock taking daily

--- src/main/kotlin/com/kcvn/spm/repository/AmoebaRepository.kt
+++ src/main/kotlin/com/kcvn/spm/repository/AmoebaRepository.kt
@@ -9,7 +9,6 @@ import com.kcvn.spm.model.tables.pojos.Amoeba
 import com.kcvn.spm.model.tables.references.AMOEBA
 import com.kcvn.spm.model.tables.references.BACKLOG_WH
 import org.jooq.DSLContext
-import org.jooq.SortOrder
 import org.jooq.TableField
 import org.jooq.impl.DSL
 import org.springframework.data.domain.Pageable
@@ -17,39 +16,69 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class AmoebaRepository(private val context: DSLContext) : SortingRepository() {
-    fun getList(request: StockTakingDailyRequest, pageable: Pageable) : Pair<List<StockTakingDailyResponse>, Int> {
-        var condition = DSL.noCondition()
+    fun getList(request: StockTakingDailyRequest, pageable: Pageable): Pair<List<StockTakingDailyResponse>, Int> {
+        val backlogWh = BACKLOG_WH
+        val amoeba = AMOEBA
+
+        val subQuery = context.select(backlogWh.LOCATION_CODE, backlogWh.PO_NUMBER)
+            .from(backlogWh)
+            .union(
+                context.select(amoeba.LOCATION_CODE, amoeba.PO_NUMBER)
+                    .from(amoeba)
+            ).asTable("loc_po")
+
+        // Field alias
+        val locationField = subQuery.field("location_code", String::class.java)
+        val poNumberField = subQuery.field("po_number", String::class.java)
+        val amoebaQtyField = DSL.max(amoeba.QTY).`as`("amoebaQty")
+        val backlogQtyField = DSL.max(backlogWh.BACKLOG_QTY).`as`("systemQty")
+        val resultField = DSL.`when`(DSL.max(amoeba.QTY).eq(DSL.max(backlogWh.BACKLOG_QTY)), "SAME")
+            .otherwise("DIFFERENT").`as`("result")
+
+        var whereCondition  = DSL.noCondition()
         if (!request.locationCode.isNullOrEmpty()) {
-            condition = condition.and(AMOEBA.LOCATION_CODE.eq(request.locationCode))
+            whereCondition  = whereCondition .and(locationField?.eq(request.locationCode))
         }
         if (!request.poNumber.isNullOrEmpty()) {
-            condition = condition.and(AMOEBA.PO_NUMBER.eq(request.poNumber))
-        }
-        if (request.isDifferentBacklog == true) {
-            condition = condition.and(AMOEBA.QTY.notEqual(BACKLOG_WH.BACKLOG_QTY))
+            whereCondition  = whereCondition .and(poNumberField?.eq(request.poNumber))
         }
 
-        val resultField = DSL.`when`(AMOEBA.QTY.eq(BACKLOG_WH.BACKLOG_QTY), "SAME")
-            .otherwise("DIFFERENT")
-            .`as`("result")
+        var havingCondition = DSL.noCondition()
+        if (request.isDifferentBacklog == true) {
+            val maxAmoebaQty = DSL.max(amoeba.QTY)
+            val maxBacklogQty = DSL.max(backlogWh.BACKLOG_QTY)
+            havingCondition = havingCondition.and(maxAmoebaQty.ne(maxBacklogQty).or(maxAmoebaQty.isNull).or(maxBacklogQty.isNull))
+        }
 
         val querySql = context.select(
-            AMOEBA.LOCATION_CODE.`as`("locationCode"),
-            AMOEBA.PO_NUMBER.`as`("poNumber"),
-            AMOEBA.QTY.`as`("amoebaQty"),
-            BACKLOG_WH.BACKLOG_QTY.`as`("systemQty"),
+            locationField,
+            poNumberField,
+            backlogQtyField,
+            amoebaQtyField,
             resultField
-        ).from(AMOEBA)
-            .join(BACKLOG_WH).on(AMOEBA.LOCATION_CODE.eq(BACKLOG_WH.LOCATION_CODE).and(AMOEBA.PO_NUMBER.eq(BACKLOG_WH.PO_NUMBER)))
-            .where(condition)
-            .orderBy(AMOEBA.LOCATION_CODE.sort(SortOrder.ASC))
+        )
+            .from(subQuery)
+            .leftJoin(backlogWh).on(
+                backlogWh.LOCATION_CODE.eq(locationField)
+                    .and(backlogWh.PO_NUMBER.eq(poNumberField))
+            )
+            .leftJoin(amoeba).on(
+                amoeba.LOCATION_CODE.eq(locationField)
+                    .and(amoeba.PO_NUMBER.eq(poNumberField))
+            )
+            .where(whereCondition )
+            .groupBy(locationField, poNumberField)
+            .having(havingCondition)
+            .orderBy(locationField?.asc())
+            .limit(pageable.pageSize)
+            .offset(pageable.offset)
 
-        val query = context.select().from(querySql)
-        val data = query.fetchInto(StockTakingDailyResponse::class.java)
-        val count = query.count()
+        val data = querySql.fetchInto(StockTakingDailyResponse::class.java)
+        val count = querySql.count()
 
         return Pair(data, count)
     }
+
 
     fun saveAll(dataList: List<Amoeba>): Int {
         if (dataList.isEmpty()) return 0

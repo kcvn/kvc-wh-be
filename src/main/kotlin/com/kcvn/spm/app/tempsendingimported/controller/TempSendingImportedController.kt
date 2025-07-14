59f4a989e5dd9969956dc39485ffcc6d84c64628
Hung Tran <quochung-tran@kcvn.com.vn>
2025-07-14 09:26:56 +0700

    support import form code and get form code list for dropdown

--- src/main/kotlin/com/kcvn/spm/app/tempsendingimported/controller/TempSendingImportedController.kt
+++ src/main/kotlin/com/kcvn/spm/app/tempsendingimported/controller/TempSendingImportedController.kt
@@ -4,6 +4,7 @@ import com.kcvn.spm.app.tempsendingimported.payload.response.TempSendingImported
 import com.kcvn.spm.app.tempsendingimported.service.TempSendingImportedService
 import com.kcvn.spm.common.payload.BasePagingResponse
 import com.kcvn.spm.common.payload.BaseResponse
+import com.kcvn.spm.common.payload.DropdownResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -20,12 +21,19 @@ class TempSendingImportedController(private val tempSendingImportedService: Temp
         return ResponseEntity(result, HttpStatus.OK)
     }
 
+    @GetMapping("form-code-dropdown")
+    fun getListFormCodeDropdown(): ResponseEntity<BaseResponse<List<DropdownResponse>>> {
+        val data = tempSendingImportedService.getListFormCodeDropdown()
+        return ResponseEntity<BaseResponse<List<DropdownResponse>>>(data, HttpStatus.OK)
+    }
+
     @PostMapping(value = ["import-excel"], consumes = ["multipart/form-data"])
     @PreAuthorize("hasAuthority(T(com.kcvn.spm.common.enums.EPermission).I_INVENTORY.value) || hasRole('ADMIN')")
     fun importTxtSending(
+        @RequestParam("formCode") formCode: String,
         @RequestPart("file") file: MultipartFile
     ): ResponseEntity<BaseResponse<Int>> {
-        val data = tempSendingImportedService.importTxtSending(file)
+        val data = tempSendingImportedService.importTxtSending(formCode, file)
         return ResponseEntity(data, HttpStatus.OK)
     }
 }
--- src/main/kotlin/com/kcvn/spm/repository/TempSendingImportedRepository.kt
+++ src/main/kotlin/com/kcvn/spm/repository/TempSendingImportedRepository.kt
@@ -3,15 +3,29 @@ package com.kcvn.spm.repository
 import com.kcvn.spm.common.constants.Constants
 import com.kcvn.spm.common.repository.SortingRepository
 import com.kcvn.spm.common.util.CommonUtils
+import com.kcvn.spm.model.tables.pojos.TempCheckingImported
 import com.kcvn.spm.model.tables.pojos.TempSendingImported
+import com.kcvn.spm.model.tables.references.TEMP_CHECKING_IMPORTED
 import com.kcvn.spm.model.tables.references.TEMP_SENDING_IMPORTED
 import org.jooq.DSLContext
 import org.jooq.TableField
 import org.jooq.impl.DSL
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.OffsetDateTime
 
 @Repository
 class TempSendingImportedRepository(private val context: DSLContext) : SortingRepository() {
+    fun findByFormCode(formCode: String): TempSendingImported? {
+        return context.selectFrom(TEMP_SENDING_IMPORTED)
+            .where(
+                TEMP_SENDING_IMPORTED.FORM_CODE.eq(formCode)
+            )
+            .fetchInto(TempSendingImported::class.java)
+            .firstOrNull()
+    }
+
     fun getList() : Pair<List<TempSendingImported>, Int> {
         val userName = CommonUtils.loggedInUser() ?: ""
         val query = context.selectFrom(TEMP_SENDING_IMPORTED).where(TEMP_SENDING_IMPORTED.CREATED_BY.eq(userName))
@@ -21,6 +35,22 @@ class TempSendingImportedRepository(private val context: DSLContext) : SortingRe
             .fetchInto(TempSendingImported::class.java)
 
         return Pair(data, count)
+    }
+
+    fun getListFormCode(): List<String> {
+        val offset = OffsetDateTime.now().offset
+        val threeDaysAgo = OffsetDateTime.of(LocalDate.now().minusDays(2), LocalTime.MIDNIGHT, offset)
+        val tomorrow = OffsetDateTime.of(LocalDate.now().plusDays(1), LocalTime.MIDNIGHT, offset)
+        return context.selectDistinct(TEMP_SENDING_IMPORTED.FORM_CODE, TEMP_SENDING_IMPORTED.CREATED_DATE)
+            .from(TEMP_SENDING_IMPORTED)
+            .where(
+                TEMP_SENDING_IMPORTED.FORM_CODE.isNotNull
+                    .and(TEMP_SENDING_IMPORTED.CREATED_DATE.ge(threeDaysAgo))
+                    .and(TEMP_SENDING_IMPORTED.CREATED_DATE.lt(tomorrow))
+            )
+            .orderBy(TEMP_SENDING_IMPORTED.CREATED_DATE.desc())
+            .fetch(TEMP_SENDING_IMPORTED.FORM_CODE)
+            .filterNotNull()
     }
 
     fun saveAll(dataList: List<TempSendingImported>): Int {
@@ -37,6 +67,7 @@ class TempSendingImportedRepository(private val context: DSLContext) : SortingRe
                         this.poNumber = data.poNumber
                         this.qty = data.qty
                         this.createdBy = CommonUtils.loggedInUser() ?: Constants.SYSTEM
+                        this.formCode = data.formCode
                     }
                 }
             ).execute()
